5507b9a39747a7b3c48de79f344e202e85358bc4
imujinu <wlsdnrhdwkd@gmail.com>
2025-05-05 23:54:47 +0900

    getServlet

--- src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -7,12 +7,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Enumeration;
 
 @WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
 public class RequestParamServlet extends HttpServlet {
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.service(req, resp);
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Enumeration<String> parameterNames = request.getParameterNames();
     }
 }
