d237bd858ddd0d678d959bf89dd2a9eb98ac9d77
imujinu <wlsdnrhdwkd@gmail.com>
2025-05-06 23:47:25 +0900

    getServlet

--- src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -14,6 +14,22 @@ public class RequestParamServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Enumeration<String> parameterNames = request.getParameterNames();
+        System.out.println("전체 파라미터 조회 - start");
+        System.out.println("ParamService");
+        request.getParameterNames().asIterator().forEachRemaining(paramName -> System.out.println(paramName + request.getParameter(paramName)));
+        System.out.println("전체 파라미터 조회 - end");
+
+        System.out.println("단일 파라미터 조회");
+        String username = request.getParameter("username");
+        String age = request.getParameter("age");
+        System.out.println(username);
+        System.out.println(age);
+        String[] usernames = request.getParameterValues("username");
+        for (String name : usernames) {
+            System.out.println(name);
+
+        }
+        response.getWriter().write("ok");
+
     }
 }
