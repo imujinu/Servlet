bcc184fa54dc83b2054ca8dbb2612a52e0d699fc
imujinu <wlsdnrhdwkd@gmail.com>
2025-05-09 23:32:18 +0900

    servlet

--- src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
@@ -1,5 +1,7 @@
 package hello.servlet.basic.request;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.annotation.WebServlet;
@@ -13,10 +15,17 @@ import java.nio.charset.StandardCharsets;
 
 @WebServlet(name="RequestBodyJsonServlet", urlPatterns = "/request-body-json")
 public class RequestBodyJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ServletInputStream inputStream = req.getInputStream();
         String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
         System.out.println("messageBody" + messageBody);
+
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+        System.out.println("helloData = " + helloData.getUsername());
+
     }
 }
